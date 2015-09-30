a2bf7d3a2b5ec66608d2b73928c9bf5249809244
Paul <paulgood247@gmail.com>
2015-09-30 15:09:55 +0000

    Lab3 Euler Problem 19

--- Lab3/Euler19.py
+++ Lab3/Euler19.py
@@ -1,5 +1,21 @@
-day in range(1,7)
-week in range(1,52)
+if(month =9 || 4 )
+  day in range(1,31)
+
+week in range(1,4)
 month in range (1, 12)
 year in range (1900, 2001)
+'''
+jan 1
+feb 2
+mar 3
+april 4
+may 5
+june 6
+jul 7
+aug 8
+sept 9 
+octo 10
+nov 11
+dec 12
+'''
 
