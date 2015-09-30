99c795a970e6a375929eb501ebacc21969a0aad3
Paul <paulgood247@gmail.com>
2015-09-30 14:43:12 +0000

    Lab3 comments

--- lab3.py
+++ lab3.py
@@ -1,8 +1,8 @@
 
-x= raw_input("Enter string: ")
-result= x[::-1]
+x= raw_input("Enter string: ") #Take in input
+result= x[::-1] #[::-1] reverses string and stores in result
 if (result ==x ):
   print 'true'
 else:
   print 'false'
-
+  
