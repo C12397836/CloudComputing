62702a4d025c62e3d66f77321dcf94f65ea182b8
Paul <paulgood247@gmail.com>
2015-09-30 15:22:25 +0000

    Lab3 Euler Problem 19

--- Lab3/Euler19.py
+++ Lab3/Euler19.py
@@ -5,9 +5,24 @@ else if(month =2):
 else:
   day in range(1,31)
 
-week in range(1,4)
+if(day==1 && month ==1 && year==1900)
+  d=7
+  
+for(;;):
+  d++
+    if(d>7):
+      d=1 && week++
+        if(week>4):
+          week=1 && month ++
+            if(month>12):
+              month=1 && year++
+                if()
+              
+
+week in range (1,4)
 month in range (1, 12)
 year in range (1900, 2001)
+d (1,7)
 '''
 jan 1
 feb 2
