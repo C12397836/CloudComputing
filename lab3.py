09e125c7779a74f21051aab0f0b29d7d7ee2bfd6
Paul <paulgood247@gmail.com>
2015-09-30 14:45:29 +0000

    Lab3 more comments

--- lab3.py
+++ lab3.py
@@ -5,4 +5,5 @@ if (result ==x ):
   print 'true'
 else:
   print 'false'
-  
+
+# I wrote the program first without any commits as my commits would not work, after i got help in lab i committed whole program. I understand the 4 commits were to follow the evolution of the program.
